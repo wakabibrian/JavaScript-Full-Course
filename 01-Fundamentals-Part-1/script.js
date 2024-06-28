930da88799708af3c9dd20306be5a437fe05256d
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-28 19:15:36 +0300

    feat: Add Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -8,3 +8,24 @@ console.log(40 + 20 + 40); //A special function for printing on the console
 // A value - A piece of data / The smallest unit of information
 console.log("Wakabi");
 console.log(23);
+
+// Variables - Stores values so that we can use them over and over again
+let firstName = "Wakabi";
+firstName = "Brian";
+console.log(firstName);
+console.log(firstName);
+
+let first = "Wakabi";
+let firstNamePerson = "Wakabi"; //camelCase
+let first_person = "Wakabi";
+
+// Rules of variables
+// Can not start with a number e.g 3years
+// Can only contain number, letters, underscore and dollar sign
+// Do not use a reserver JS Keyword e.g new, function
+
+const PI = 3.1415; // Constants do not change
+
+// Make your variables descriptive
+let myFirstJob = "Mechanic";
+let myCurrentJob = "Programmer";
