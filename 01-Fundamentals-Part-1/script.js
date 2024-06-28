7c0e0e75dd240b8e6fa895b42dccfbc7037df8e7
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-28 08:31:55 +0300

    feat: Add external JavaScript

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,5 @@
+// External JavaScript helps separation of concerns making it easier to maintain code
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log(40 + 20 + 40); //A special function for printing on the console
