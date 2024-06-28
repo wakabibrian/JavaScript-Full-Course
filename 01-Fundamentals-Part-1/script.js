34365dbc14d91dd53e2309a8d6fcbd3e2e375561
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-28 18:44:00 +0300

    feat: Add values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -3,3 +3,8 @@ let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 20 + 40); //A special function for printing on the console
+
+// Values and Variables
+// A value - A piece of data / The smallest unit of information
+console.log("Wakabi");
+console.log(23);
