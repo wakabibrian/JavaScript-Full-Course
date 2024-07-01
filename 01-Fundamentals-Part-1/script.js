124bc32702e23600a64d4810b11c2cfcbbab3239
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-01 07:39:48 +0300

    feat: Add exercise - Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -337,3 +337,21 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+
+// Exercise
+/*
+1. If your country's population is greater than 33 million, log a string like this to the console: "Portugal's population is 22 million below average" (the 22 is the average of 33 minus the country's population).
+
+2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+*/
+
+let population = 45.9;
+const country = "Uganda";
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+}
