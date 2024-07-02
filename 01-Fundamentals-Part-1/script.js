a44661effa2c4d44c1912bfd2ba89a4ef28adfa2
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-03 01:24:59 +0300

    feat: Add switch statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -535,3 +535,26 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 */
 
 //=============================== The switch Statement
+const day = "monday";
+switch (day) {
+  case "monday": // day === "monday"
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day");
+}
