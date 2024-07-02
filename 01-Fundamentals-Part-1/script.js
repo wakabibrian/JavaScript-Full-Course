3c4bea804441e566a17f250f4a970ed0688c1f03
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-03 01:32:36 +0300

    feat: Add if statement - switch statement equivalent

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -535,7 +535,7 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 */
 
 //=============================== The switch Statement
-const day = "monday";
+const day = "sunday";
 switch (day) {
   case "monday": // day === "monday"
     console.log("Plan course structure");
@@ -557,4 +557,20 @@ switch (day) {
     break;
   default:
     console.log("Not a valid day");
+}
+
+// if statement equivalent
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend");
+} else {
+  console.log("Not a valid day");
 }
