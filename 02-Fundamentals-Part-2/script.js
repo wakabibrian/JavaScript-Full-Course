250781eab04c8151e30487be4805d95df16ffd5c
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-03 08:50:42 +0300

    feat: Add strict mode examples

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,1 +1,12 @@
+//=============================== Activating Strict Mode
+
 "use strict"; //Helps to write secure JavaScript code.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio"; // Reserved JS word for future
+// const private = 453;
