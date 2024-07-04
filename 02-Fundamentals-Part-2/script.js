b345e23b8d3c3ececba2d5236c58f1aa6a248cdf
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-04 08:13:30 +0300

    feat: Add function expression

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -47,3 +47,10 @@ function calcAge1(birthYear) {
 
 const age1 = calcAge1(1990);
 console.log(age1);
+
+// Function expression(using an anonymous function) - produces a value
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1990);
+console.log(age2);
