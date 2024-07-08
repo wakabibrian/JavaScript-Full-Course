957c9e741225dd3a272baeef5e933355b40eda1b
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-08 07:33:01 +0300

    feat: Finish coding challenge

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -146,3 +146,36 @@ To calculate average of 3 values, add them all together and divide by 3
 To check if number A is at least double number B, check for A >= 2 * B.
 Apply this to the team's average scores
 */
+
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+const calcAverage = (score1, score2, score3) => {
+  return (score1 + score2 + score3) / 3;
+};
+
+// 2. Use the function to calculate the average for both teams
+// // Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+
+// Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+const checkWinner = (avgDolhins, avgKoalas) => {
+  if (avgDolhins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolhins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+  } else {
+    console.log(
+      `No one wins. dolphinsScore: ${avgDolhins} vs koalasScore: ${avgKoalas}`
+    );
+  }
+};
+
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
+checkWinner(scoreDolphins, scoreKoalas);
