e3caf09cd65b66051c3e040beadca528f39dfcc2
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-11 18:03:38 +0300

    refactor: start coding challenge

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -408,6 +408,7 @@ console.log(
 */
 
 //=============================== Object Methods
+/*
 const wakabi = {
   firstName: "Wakabi",
   lastName: "Brian",
@@ -450,3 +451,21 @@ console.log(wakabi.age);
 console.log(wakabi.getSummary());
 
 // Arrays are also objects/special kind of objects since they have methods which can be used to manipulate them
+*/
+
+//=============================== Coding Challenge #3
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall
+*/
