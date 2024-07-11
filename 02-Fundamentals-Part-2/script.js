79aa2ca21b42013bab43d8518e6ef92a32d04f62
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-11 19:48:49 +0300

    feat: Add for loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -516,3 +516,18 @@ console.log(
 */
 
 //=============================== Iteration: The for Loop
+// console.log("Lifting weight repetition 1");
+// console.log("Lifting weight repetition 2");
+// console.log("Lifting weight repetition 3");
+// console.log("Lifting weight repetition 4");
+// console.log("Lifting weight repetition 5");
+// console.log("Lifting weight repetition 6");
+// console.log("Lifting weight repetition 7");
+// console.log("Lifting weight repetition 8");
+// console.log("Lifting weight repetition 9");
+// console.log("Lifting weight repetition 10");
+
+// for loop keeps running while the condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weight repetition ${rep}🏋️`);
+}
