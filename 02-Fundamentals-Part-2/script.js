543d9a4b4c9a30d1cb123c3d34f31927c1763a6a
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-13 11:40:11 +0300

    feat: Add coding challenge

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,7 +1,7 @@
 //=============================== Activating Strict Mode
-/*
 "use strict"; //Helps to write secure JavaScript code.
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -666,3 +666,31 @@ added together
 length of the array (because that's the number of elements)
 4.3. Call the function with the 'totals' array
 */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = (bill) => {
+  if (bill >= 50 && bill <= 300) {
+    return 0.15 * bill;
+  } else {
+    return 0.2 * bill;
+  }
+};
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage(totals));
