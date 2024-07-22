0cabb9debd4dc015a9b88c87ef7324f5fcaf69c5
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-22 19:01:03 +0300

    feat: Change background color

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -19,13 +19,19 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // When the player wins
   } else if (guess === secreteNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    // When guess is too high
   } else if (guess > secreteNumber) {
     document.querySelector('.message').textContent = '📈 Too High!';
-
     if (score > 1) {
       score--;
       document.querySelector('.score').textContent = score;
@@ -33,6 +39,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secreteNumber) {
     document.querySelector('.message').textContent = '📉 Too Low!';
 
