d7c9b3c90d79d005d1d4335288bdf66ba4af041d
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-23 19:38:55 +0300

    chore: refactor code

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -4,17 +4,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 
