be9ddd8d3b57f9d74910277844b505694570e754
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-25 22:23:24 +0300

    feat: Add Rolling a dice

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,9 +3,37 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //Faster
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // Display dice roll
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // Check if it is a 1.
+  if (dice !== 1) {
+    //Add to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //Change later
+  } else {
+    // Switch players
+  }
+});
