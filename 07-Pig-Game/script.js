603dbfc32b7e04fba233ccb6f6c6b8c4fb75909f
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-25 08:40:05 +0300

    feat: Add starting conditions and selecting elements

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,6 +3,9 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //Faster
+const diceEl = document.querySelector('.dice');
 
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
