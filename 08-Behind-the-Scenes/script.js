2612dc930032cdf2152a780a668078d1d429702f
Wakabi Brian <wakabibrian95@gmail.com>
2024-07-28 17:29:56 +0300

    feat: Add scoping

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  //   console.log(firstName); // From global scope
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millennial = true;
+      //   Creating new variable with the same name as outer scope's variable
+      const firstName = 'Steven';
+      //   Reassigning outer scope's variable
+      output = 'New Output!'; // Affects the outer scope
+      const str = `Oh, and you're a millennial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str); Throws an error
+    console.log(millennial); //var variables are function scoped not block scoped
+    // console.log(add(2, 3)); // Throws an error, because functions are block scoped
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Brian';
+calcAge(1990);
+// console.log(age); //Throws  an error due to scoping
+// printAge(); //Throws  an error due to scoping
