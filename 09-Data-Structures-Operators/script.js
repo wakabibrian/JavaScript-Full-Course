04746d19c08248a258cf396bb81baf7766c1341a
Wakabi Brian <wakabibrian95@gmail.com>
2024-08-07 06:36:48 +0300

    feat: Add default values - destructuring

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -70,3 +70,7 @@ console.log(starter, mainCourse);
 const nested = [2, 4, [5, 6]];
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
