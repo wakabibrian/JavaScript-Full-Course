af443097ba52c386077d9f79c74238046a8aeba1
Wakabi Brian <wakabibrian95@gmail.com>
2024-08-11 05:37:25 +0300

    feat: Add OR assignment operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -271,3 +271,22 @@ console.log(guestsCorrect);
 */
 
 // =============================  Logical Assignment Operators
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// OR assignment operator - Assigns a value to a variable if that value is currently falsy
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+console.log(rest1);
+console.log(rest2);
