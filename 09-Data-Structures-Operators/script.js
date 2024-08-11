2d8fb1a15c915e378cfe901313fb9bef64cd88f3
Wakabi Brian <wakabibrian95@gmail.com>
2024-08-11 05:08:50 +0300

    feat: Add && operator short-circuiting

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -229,7 +229,7 @@ restaurant.orderPizza('mushrooms'); //Others are optional
 
 // ============================= Short Circuiting (&& and ||)
 // Logic Operators can use ANY datatype, return ANY datatype, and can do short-circuiting
-// || operator short-circuiting - If the first value is a truth value it will immediately return that truth value
+// || (OR) operator short-circuiting - If the first value is a truth value it will immediately return that truth value
 console.log(3 || 'Jonas'); //Returns 3
 console.log('' || 'Jonas'); //Returns Jonas
 console.log(true || 0); //Returns true
@@ -243,3 +243,9 @@ console.log(guests1);
 
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
+
+// && (AND) operator short-circuiting - If the first value is a false value it will immediately return that false value
+console.log(0 && 'Jonas'); //Returns 0
+console.log(7 && 'Jonas'); //Returns Jonas
+
+console.log('Hello' && 23 && null && 'Jonas'); // Returns null
