24949ad4f16a78065d95e26d6ea138de6b76ae1d
Wakabi Brian <wakabibrian95@gmail.com>
2024-08-20 20:44:58 +0300

    feat: Add example

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -439,3 +439,11 @@ if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 // With optional chaining
 console.log(restaurant.openingHours.mon?.open);
 console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we are open at ${open}`);
+}
