c8c4c4eac9247eb6b05dc49cdad5cc5fbb8912ac
Wakabi Brian <wakabibrian95@gmail.com>
2024-08-26 05:35:45 +0300

    feat: Add sets example

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -594,3 +594,14 @@ console.log(ordersSet);
 
 // Looping elements in a set
 for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('wakabibrian').size);
