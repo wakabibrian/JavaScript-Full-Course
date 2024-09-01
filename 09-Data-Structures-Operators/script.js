525e4ca9a0a927d0065fff30705931c8ea7fc8fb
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-01 04:46:47 +0300

    feat: Add better way of writing new map

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -643,3 +643,13 @@ console.log(rest.get(arr));
 */
 
 // ===================== Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
