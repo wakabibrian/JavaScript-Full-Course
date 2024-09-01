bce761c63f055094e2f20483d87c99f6f0ff4826
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-01 04:20:14 +0300

    feat: Add set/add and get data from a map

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -609,3 +609,22 @@ console.log(new Set('wakabibrian').size);
 */
 
 // ===================== Maps: Fundamentals
+// A map is a data structure that we use to map values to keys
+// In a map, the keys can have any data type while in objects they are always strings
+const rest = new Map(); // New map
+rest.set('name', 'Classico Italiano'); // Add data to a map
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal')); // Adds and returns a map
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :C');
+
+console.log(rest.get('name')); // Read data from a map
+console.log(rest.get(true));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
