d2b27b4f00ed6406f5f75f8440af5e1b2a17c905
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-02 20:30:50 +0300

    feat: Add introduction to strings

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -737,3 +737,12 @@ for (const [minutes, event] of gameEvents.entries()) {
 */
 
 // ===================== Working With Strings - Part 1
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
