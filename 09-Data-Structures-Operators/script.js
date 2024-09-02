33eae2566069fb81437ad7f0e6dc137341d3075e
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-02 20:44:23 +0300

    feat: Add string methods

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -746,3 +746,18 @@ console.log('B737'[0]);
 
 console.log(airline.length);
 console.log('B737'.length);
+
+// String Methods
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+console.log(airline.indexOf('portugal')); //Not found, -1
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
