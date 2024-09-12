ed9d2b9916e80579eed5385e7b0c705c7c37ee6e
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-12 08:11:13 +0300

    feat: Add a Higher-order function

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -78,3 +78,31 @@ checkIn(flight, wakabi);
 // A function that receives another function as an argument, returns a new function, or both
 
 // =============================  Functions Accepting Callback Functions
+// Functions that accept other functions as inputs
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
