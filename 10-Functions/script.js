6f51bcdd4e78b1a579891638292f7db36540e5b5
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-14 09:19:17 +0300

    feat: Add bind with Event listeners

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -193,3 +193,17 @@ bookEW(23, 'Steven Williams');
 const bookEW23 = book.bind(eurowings, 23);
 bookEW23('Wakabi Brian');
 bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
