be57ec31010db1dcbe4aca10e776fca46078bbce
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-14 09:34:35 +0300

    feat: Add partial Application example

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -207,3 +207,13 @@ lufthansa.buyPlane = function () {
 document
   .querySelector('.buy')
   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial Application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23); // null -> This Keyword
+// addVAT = value => value + value * 0.23
+
+console.log(addVAT(100));
+console.log(addVAT(23));
