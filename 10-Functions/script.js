2f9af9de7f657daa5014bbae3eb97754fce58e11
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-24 05:58:42 +0300

    feat: Add console.dir() - shows the closure of a function

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -366,3 +366,5 @@ booker();
 // 2. A closure gives a function access to all the variables of its parent function, even after that parent function has returned. The  function keeps a reference to its outer scope, which preserves the scope chain throughout time
 // 3. A closure makes sure that a function doesn’t loose connection to variables that existed at the function’s birth place
 // 4. A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables that were present in the environment where the function was created
+
+console.dir(booker);
