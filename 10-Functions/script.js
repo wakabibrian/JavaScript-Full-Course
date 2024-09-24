2e33b1db48f9f36821bae3e7efdb130cc76b63fc
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-24 05:55:47 +0300

    feat: Add closure definitions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -360,3 +360,9 @@ booker();
 
 // Explanation on the booker()
 // booker() tries to increase the passengerCount function, but the secureBooking() is off the execution stack, therefore the booker() will look into the closure even before it looks in the scope chain.
+
+// Closure definitions
+// 1. A closure is the closed-over variable environment of the execution context in which a function was created, even after that execution context is gone.
+// 2. A closure gives a function access to all the variables of its parent function, even after that parent function has returned. The  function keeps a reference to its outer scope, which preserves the scope chain throughout time
+// 3. A closure makes sure that a function doesn’t loose connection to variables that existed at the function’s birth place
+// 4. A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables that were present in the environment where the function was created
