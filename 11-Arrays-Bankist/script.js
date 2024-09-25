38422307f29d9c451872611384a39c65328705da
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-25 21:47:28 +0300

    feat: Add coding challenge

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -214,3 +214,23 @@ Test data:
 § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 */
+
+const checkDogs = function (arr1, arr2) {
+  // console.log(arr1, arr2);
+  const arr1Copy = arr1.slice();
+  arr1Copy.splice(0, 1);
+  arr1Copy.splice(-2);
+
+  // const arrayCombined = [...arr1Copy, ...arr2];
+  const arrayCombined = arr1Copy.concat(arr2);
+
+  arrayCombined.forEach(function (age, i) {
+    if (age >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
