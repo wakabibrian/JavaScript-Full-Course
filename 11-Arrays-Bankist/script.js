10ae1739a51e126aaece6ccf9296f8590913e6be
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-26 05:25:15 +0300

    chore(new lesson): Start reduce method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -281,6 +281,7 @@ console.log(movementsDescriptions);
 */
 
 // =============================  The filter Method
+/*
 // It is used to filter elements that satisfy a certain condition
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const deposits = movements.filter(function (mov) {
@@ -300,3 +301,6 @@ console.log(deposits2);
 // challenge - withdraws
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+*/
+
+// =============================  The reduce Method
