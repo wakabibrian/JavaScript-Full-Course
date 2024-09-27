6a379a53f45f9c591c6fd127fe31179584bbf2e9
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-27 05:20:24 +0300

    feat: Add summary - total incomes, out, and interest

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -82,9 +82,32 @@ addMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsername = function (accs) {
   accs.forEach(function (acc) {
@@ -377,7 +400,7 @@ const euroToUSD = 1.1;
 const totalDepositUSD = movements
   .filter(mov => mov > 0)
   .map((mov, i, arr) => {
-    console.log(arr);
+    // console.log(arr);
     return mov * euroToUSD;
   })
   // .map(mov => mov * euroToUSD)
