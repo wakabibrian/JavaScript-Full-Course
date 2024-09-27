56ef0199b11a9ed1c69078f004434bd24b0628fe
Wakabi Brian <wakabibrian95@gmail.com>
2024-09-27 04:27:45 +0300

    feat: Add coding challenge

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -348,3 +348,21 @@ Test data:
   Data 1: [5, 2, 4, 1, 15, 8, 3]
   Data 2: [16, 6, 10, 5, 6, 1, 4]
 */
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
+
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(adults);
+
+  // const averageHumanAge = humanAges.reduce((acc, age) => acc + age, 0) / ages.length; (2+3)/2 => 2/2 + 3/2
+  const averageHumanAge = humanAges.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+  console.log(averageHumanAge);
+};
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
