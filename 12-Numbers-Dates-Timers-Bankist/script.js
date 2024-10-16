aa3aacbdc79e5d46be8d9aa0019ffa340a5d4c15
Wakabi Brian <wakabibrian95@gmail.com>
2024-10-16 05:43:30 +0300

    feat: Add sqrt, max and min

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -294,3 +294,11 @@ console.log(Number.isInteger(23 / 0)); //false
 */
 
 // =============================  Math and Rounding
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+console.log(Math.max(5, 18, 23, 11, 10));
+console.log(Math.max(5, 18, '23', 11, 10)); //23 - Does type coercion
+console.log(Math.max(5, 18, '23px', 11, 10)); //NaN - Doesn't parse
+
+console.log(Math.min(5, 18, 23, 11, 10));
