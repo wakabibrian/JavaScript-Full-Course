c8bb8278fe84af9728c7d2bc969fb65bc101837b
Wakabi Brian <wakabibrian95@gmail.com>
2024-10-28 15:23:19 +0300

    feat: Add options - units

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -554,10 +554,15 @@ console.log(days1);
 // =============================   Internationalizing Numbers (Intl)
 const num = 3884764.23;
 
-console.log('US: ', new Intl.NumberFormat('en-US').format(num));
-console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
-console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
+const options = {
+  style: 'unit',
+  unit: 'celsius',
+};
+
+console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log(
   navigator.language,
-  new Intl.NumberFormat(navigator.language).format(num)
+  new Intl.NumberFormat(navigator.language, options).format(num)
 );
