bbe1e71d84045b839858e577eb7d884ddf602e64
Wakabi Brian <wakabibrian95@gmail.com>
2024-10-28 15:19:59 +0300

    feat: Add formating numbers

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -552,3 +552,12 @@ console.log(days1);
 // =============================   Internationalizing Dates (Intl)
 
 // =============================   Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+console.log('US: ', new Intl.NumberFormat('en-US').format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE').format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY').format(num));
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language).format(num)
+);
