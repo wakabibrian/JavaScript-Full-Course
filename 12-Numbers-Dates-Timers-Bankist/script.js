576e73fa5443946f1b49a245ffbb8b0e7681afe3
Wakabi Brian <wakabibrian95@gmail.com>
2024-10-29 07:43:18 +0300

    feat: Add setInterval

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -598,6 +598,8 @@ console.log(
 // setInterval - keeps running - forever until we stop it
 // setTimeout - used to execute some code, in the future
 // Asynchronous JavaScript
+
+// setTimeout
 const ingredients = ['olive', 'spinach'];
 const pizzaTimer = setTimeout(
   (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}🍕`),
@@ -607,3 +609,9 @@ const pizzaTimer = setTimeout(
 console.log('Waiting...');
 
 if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval - runs forever
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
