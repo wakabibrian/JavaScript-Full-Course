5a2285ecc1686ec2fb6b8e0ab019a869baea617f
Wakabi Brian <wakabibrian95@gmail.com>
2024-11-20 00:49:53 +0300

    feat: Add Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,4 +1,7 @@
 'use strict';
+'use strict';
+// ================= Selecting and Manipulating Elements
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
@@ -6,3 +9,13 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
+*/
+
+// ================= Handling Click Events
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+});
