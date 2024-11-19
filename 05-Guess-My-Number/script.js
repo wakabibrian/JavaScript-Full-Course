ea5caa2f66b674001958ef42954e969e2d16bc96
Wakabi Brian <wakabibrian95@gmail.com>
2024-11-20 02:05:25 +0300

    feat: Implement high score

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -13,6 +13,7 @@ document.querySelector('.guess').value = 23;
 
 let secreteNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +29,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // If guess is too high
   } else if (guess > secreteNumber) {
