fe10e2f67bdd063b974497e7eb62eeba17fd9e15
Wakabi Brian <wakabibrian95@gmail.com>
2024-11-23 14:35:38 +0300

    feat: Selected all elements

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,8 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  console.log(btnsShowModal[i].textContent);
